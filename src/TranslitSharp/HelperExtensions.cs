c373ff983576cad6a1b5ac82ae70f659c24761a3
PBourdoumpas <58686990+PBourdoumpas@users.noreply.github.com>
2020-07-14 14:32:24 +0300

    Hotfix/fixed two character token hash generator (#2)
    * fixed two character token hash generator
    
    * removed an unused constant
    
    * altered some constant values in the hash generator

--- src/TranslitSharp/HelperExtensions.cs
+++ src/TranslitSharp/HelperExtensions.cs
@@ -40,17 +40,15 @@ namespace TranslitSharp
             return GetHashCodeFromCharacters(text, text.Length);
         }
 
-        private const int hash = 17;
-        private const int seed = 31;
-        private const int first_hash_seed = hash * seed;
+        private const int hash = 5381;
+        private const int seed = 1566083941;
 
         public static int GetHashCodeFromCharacters(this IEnumerable<char> characters, int length)
         {
             if (length <= 0) return 0;
             if (length == 1) return characters.First().GetHashCode();
-            if (length == 2) return seed * (first_hash_seed + characters.First().GetHashCode()) + characters.Last().GetHashCode();
 
-            int hash1 = 5381;
+            int hash1 = hash;
             int hash2 = hash1;
 
             int i = 0;
@@ -64,15 +62,7 @@ namespace TranslitSharp
                 if (++i == 2) i = 0;
             }
 
-            return hash1 + (hash2 * 1566083941);
-
-            //unchecked
-            //{
-            //    int hash = 17;
-            //    foreach (var character in characters)
-            //        hash = 31 * hash + character.GetHashCode();
-            //    return hash;
-            //}
+            return hash1 + (hash2 * seed);
         }
 
         public static bool IsAscii(this char c, bool excludeExtendedAscii)
