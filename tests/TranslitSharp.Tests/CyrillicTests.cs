79f5d229e1acb781d6665c27a4a23b2d1b8acb20
Theodore Karropoulos <tkarropoulos@vivawallet.com>
2021-01-19 12:44:32 +0200

    Support of cyrillic characters

---
+++ tests/TranslitSharp.Tests/CyrillicTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace TranslitSharp.Tests
+{
+    public class CyrillicTests
+    {
+        [Theory]
+        [InlineData("это кириллица", "eto kirillica")]
+        [InlineData("Афина", "Afina")]
+        [InlineData("Атина", "Atina")]
+        [InlineData("кой кой е", "koj koj e")]
+        [InlineData("ЎЌЉЪ", "UKLЪ")]
+        [InlineData("суЇш ыхћ", "suIs yhc")]
+        public void Should_Transliterate_Gyrillic_To_Latin(string input, string expected)
+        {
+            var transliterator = new Transliterator(x => x
+                .ConfigureCyrillicToLatin());
+
+            var result = transliterator.Transliterate(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
